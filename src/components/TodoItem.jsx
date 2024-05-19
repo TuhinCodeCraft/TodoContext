651bc0d4bec1a291029022728e0cba3ec367d21f
tuhin56789 <tuhinghosh0169@gmail.com>
2024-05-19 21:41:12 +0530

    refactor: Remove unnecessary code in TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -14,7 +14,6 @@ function TodoItem({ todo }) {
     const toggleCompleted = () => {
         toggleComplete(todo.id);
     };
-
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300 text-black ${
