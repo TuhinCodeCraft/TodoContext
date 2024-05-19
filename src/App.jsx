9b5e08610cc9f5151cea72da1bc84147136c5867
tuhin56789 <tuhinghosh0169@gmail.com>
2024-05-19 19:01:56 +0530

    style: Update background color and remove unnecessary CSS class in Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-      <div className="bg-[#172842] max-h-screen py-8 rounded-lg">
+      <div className=" max-h-screen py-8 rounded-lg">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white bg-[#2e4b77]">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Add Your Todos Here</h1>
           <div className="mb-4">
